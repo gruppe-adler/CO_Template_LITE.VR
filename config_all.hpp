be28a6f6e9f16231e85fa1cdf59d4de6faf820bd
McDiod <hobbeldobbel@gmail.com>
2016-11-04 20:18:37 +0100

    added to git

---
+++ config_all.hpp
@@ -0,0 +1,43 @@
+//Parameter
+#include "ga_system\sys_config\params.hpp"
+
+//Sounds
+#include "ga_system\sys_config\sound.hpp"
+
+//Loadouts
+#include "loadouts\loadouts.hpp"
+
+//Functions
+class CfgFunctions
+{
+
+  #include "functions\geometry\cfgFunctions.hpp"
+  #include "functions\spawnhelp\cfgFunctions.hpp"
+  #include "functions\conview\cfgFunctions.hpp"
+  //#include "functions\mission\cfgFunctions.hpp"
+  #include "functions\usage\cfgFunctions.hpp"
+  #include "functions\dial\cfgFunctions.hpp"
+  #include "functions\external\cfgFunctions.hpp"
+  #include "functions\client\cfgFunctions.hpp"
+  #include "functions\system\cfgFunctions.hpp"
+  #include "functions\hc\cfgFunctions.hpp"
+  #include "functions\creating\cfgFunctions.hpp"
+  #include "f3cache\cfgFunctions.hpp"
+  #include "node_modules\grad-loadout\cfgFunctions.hpp"
+  #include "node_modules\grad-customGear\cfgFunctions.hpp"
+};
+
+//grad-customGear
+class grad_customGear {
+  #include "USERSCRIPTS\customGear.hpp"
+};
+
+//RSCTitles
+#include "ga_system\sys_dlg\rsctitles.hpp"
+
+//Dialogs
+#include "ga_system\sys_dlg\dialog.hpp"
+#include "node_modules\grad-customGear\saveDialog\dialog.hpp"
+
+//CfgRemote
+#include "ga_system\sys_config\CfgRemote.hpp"
